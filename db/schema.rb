f41485c25263b7b8c7fbd2bbaa53da588f08ee41
Florence Schwartz <schflo@gmail.com>
2019-09-05 14:03:43 +0200

    created model tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_195058) do
+ActiveRecord::Schema.define(version: 2019_09_05_115750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.bigint "user_id"
