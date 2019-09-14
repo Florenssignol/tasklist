e0b78f7116590ed8f2c1f1c621a4a36e43a2de9f
Florence Schwartz <schflo@gmail.com>
2019-09-14 16:02:23 +0200

    system tests working for lists

--- test/system/lists_test.rb
+++ test/system/lists_test.rb
@@ -2,40 +2,69 @@ require "application_system_test_case"
 
 class ListsTest < ApplicationSystemTestCase
   setup do
-    @list = lists(:one)
+    @user = User.create!(email: 'foo@foo.mail', password: 'foofoo', name: 'foofoo')
+    @user_2 = User.create!(email: 'toto@foo.mail', password: 'foofoo', name: 'toto')
+    @list = @user.lists.create(name: 'user list name')
   end
 
-  test "visiting the index" do
+  def log_in_user 
     visit lists_url
-    assert_selector "h1", text: "Lists"
-  end
+    fill_in 'user[email]', with: 'foo@foo.mail'
+    fill_in 'user[password]', with: 'foofoo'
+    click_on 'Log in'
+  end 
 
-  test "creating a List" do
+  test 'require authenticated user to access lists' do
     visit lists_url
-    click_on "New List"
-
-    click_on "Create List"
-
-    assert_text "List was successfully created"
-    click_on "Back"
+    assert_text 'Log in'
   end
 
-  test "updating a List" do
-    visit lists_url
-    click_on "Edit", match: :first
-
-    click_on "Update List"
-
-    assert_text "List was successfully updated"
-    click_on "Back"
+  test 'as a logged user I can see my lists' do
+    log_in_user()
+    assert_text 'Your Lists'
+    assert_text 'User list name'
   end
 
-  test "destroying a List" do
-    visit lists_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "List was successfully destroyed"
+  test 'as a logged user I can create a list' do
+    log_in_user()
+    click_on 'Create a new list'
+    fill_in 'list[name]', with: 'Books'
+    fill_in 'list[description]', with: 'most awesome books'
+    click_on 'submit'
+    assert_text 'List was successfully created.'
+    assert_equal(@user.lists.count(), 2, true) 
+     #1 created in this test and one in the setup
   end
+
+  test 'as a logged user I can see the tasks of a list' do 
+    log_in_user()
+    click_on 'View details'
+    assert_text 'Tasks'
+  end 
+
+  test 'as a logged user I can delete a list' do 
+    log_in_user()
+    click_on 'View details'
+    click_on 'Delete list'
+    page.driver.browser.switch_to.alert.accept
+    assert_text 'List was successfully destroyed.'
+    assert_equal(@user.lists.count(), 0, true)
+  end
+
+  test 'as a logged user I can share a list and list is successfully shared' do 
+    log_in_user()
+    click_on 'View details'
+    click_on 'Give list access to a friend'
+    select 'toto', :from => 'list_access[user_id]'
+    click_on 'submit'
+    assert_text 'List was successfully shared.'
+    click_on 'Logout'
+    click_on 'sign in'
+    fill_in 'user[email]', with: 'toto@foo.mail'
+    fill_in 'user[password]', with: 'foofoo'
+    click_on 'Log in'
+    assert_text 'Lists shared with you'
+    assert_text 'User list name'
+  end
+
 end
